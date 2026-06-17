95e30e9a8efc63d60abf2ddbf84eb0a64f9153db
Vlantix <bonzakent16@gmail.com>
2026-06-17 14:59:01 +0800

    feat: remove DEBUG configuration from environment variables in config

--- backend/config.py
+++ backend/config.py
@@ -5,13 +5,12 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    DEBUG = os.getenv("DEBUG", "FALSE").upper() == "TRUE"
 
     DATABASE_URL = os.getenv("DATABASE_URL")
 
     SUPABASE_URL = os.getenv("SUPABASE_URL")
     SUPABASE_KEY = os.getenv("SUPABASE_KEY")
     SUPABASE_BUCKET = os.getenv("SUPABASE_BUCKET")
-
+    
     RESEND_API_KEY = os.getenv("RESEND_API_KEY")
     RESEND_SENDER_EMAIL = os.getenv("RESEND_SENDER_EMAIL")
